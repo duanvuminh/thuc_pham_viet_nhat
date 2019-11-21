ac798c0be617400dcd67460fc502338688729a5b
manhlm <manhlm@evolable.asia>
2019-11-21 20:27:32 +0900

    change Menu bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,88 @@
 <template>
   <ais-instant-search-ssr>
     <v-row>
+
+      <!--Tao link toolbar-->
+      <v-toolbar
+        :extended="extended"
+        :prominent="prominent"
+        :dense="dense"
+        :collapse="collapse"
+        :flat="flat"
+        :src="bg ? 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg' : undefined"
+        :extension-height="extensionHeight"
+      >
+        <template v-if="extendedSlot" #extension>
+          <v-toolbar-items>
+            <v-list>
+              <template v-slot:activator>
+                <v-list-item-title>Users</v-list-item-title>
+              </template>
+
+              <v-list-group
+                no-action
+                sub-group
+                value="true"
+              >
+                <template v-slot:activator>
+                  <v-list-item-content>
+                    <v-list-item-title>Admin</v-list-item-title>
+                  </v-list-item-content>
+                </template>
+
+                <v-list-item
+                  v-for="(admin, i) in admins"
+                  :key="i"
+                  link
+                >
+                  <v-list-item-title v-text="admin[0]"></v-list-item-title>
+                  <v-list-item-icon>
+                    <v-icon v-text="admin[1]"></v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list-group>
+            </v-list>
+          </v-toolbar-items>
+        </template>
+
+        <v-toolbar-title>Title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+          <v-btn text>Link 1</v-btn>
+          <v-btn text>Link 2</v-btn>
+          <v-btn text>Link 3</v-btn>
+        </v-toolbar-items>
+
+        <template v-if="$vuetify.breakpoint.smAndUp">
+          <v-btn icon>
+            <v-icon>mdi-export-variant</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-delete-circle</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>mdi-plus-circle</v-icon>
+          </v-btn>
+        </template>
+      </v-toolbar>
+
+      <!--Design thanh search-->
       <v-col cols="12">
         <v-toolbar color="orange accent-1">
           <v-icon class="pr-5" color="purple  accent-3">pets</v-icon>
-          <ais-search-box style="flex:1" />
-          <!-- <template v-slot:extension>
-        <v-spacer></v-spacer>
-        <v-btn text>Địa điểm</v-btn>
-        <v-btn text>Giá cả</v-btn>
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-          </template>-->
+          <ais-search-box style="flex:1">
+          </ais-search-box>
         </v-toolbar>
       </v-col>
+
+      <!-- Danh sách category  -->
       <v-col cols="12" md="3">
         <ais-refinement-list attribute="type" :limit="100" />
       </v-col>
+
+      <!-- Danh sách các bài viết -->
       <v-col cols="12" md="8">
         <ais-hits class="mb-5">
           <template slot-scope="{ items }">
@@ -132,9 +197,40 @@ export default {
     AisPoweredBy,
     AisPagination
   },
-  data() {
-    return {};
-  },
+  data: () => ({
+    items: [
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me 2' },
+    ],
+    admins: [
+      ['Management', 'people_outline'],
+      ['Settings', 'settings'],
+    ],
+    cruds: [
+      ['Create', 'add'],
+      ['Read', 'insert_drive_file'],
+      ['Update', 'update'],
+      ['Delete', 'delete'],
+    ],
+    disabled: true,
+    absolute: true,
+    openOnHover: true,
+    value: true,
+    closeOnClick: true,
+    closeOnContentClick: true,
+    offsetX: false,
+    offsetY: true,
+    extended: true,
+    extendedSlot: true,
+    prominent: true,
+    dense: true,
+    collapse: true,
+    flat: true,
+    bg: true,
+    extensionHeight: 48,
+  }),
   head() {
     return {
       link: [
