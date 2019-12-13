4274b92774797d7cceb5418eef94b667ea8afe3b
duanvuminh <duanvuminh@gmail.com>
2019-12-13 14:27:50 +0900

    fixed enablePersistence dont work

--- plugins/fireinit.js
+++ plugins/fireinit.js
@@ -15,10 +15,10 @@ var config = {
 }
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
-  firebase.app()
+}
+firebase.app()
   .firestore()
   .enablePersistence({ synchronizeTabs: true })
-}
 export default ({ store }) => {
   firebase.auth().onAuthStateChanged((user) => {
     console.log("firebase work")
