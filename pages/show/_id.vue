09977260be4a0f30c25f11786d11b3d88970b6db
DUANVM <duanvuminh@gmail.com>
2020-01-16 10:25:42 +0900

    fix syntax

--- pages/show/_id.vue
+++ pages/show/_id.vue
@@ -292,7 +292,7 @@ ${str}
             `;
             });
             this.tabs[1].webo= this.webo
-            this.tabs[1].text: `## ${response.data[0].word} 
+            this.tabs[1].text= `## ${response.data[0].word} 
 ### ${response.data[0].phonetic}
 ${strmean}         
             `
