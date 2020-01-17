d3ee2d140eb4bea4db19a4b222502a9a6c80432f
duanvm <duanvm@evolable.asia>
2020-01-17 22:51:55 +0900

    ssr

--- components/Strockes.vue
+++ components/Strockes.vue
@@ -1,5 +1,9 @@
 <template>
-  <div ref="object" class="d-flex flex-row mb-6 flex-wrap"></div>
+  <div class="d-flex flex-row mb-6 flex-wrap" style="overflow-x:auto">
+    <div class="d-flex">
+    <div v-for="(item,index) in kanji" :key="index" :ref="`object${index}`"></div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -10,55 +14,54 @@ export default {
     };
   },
   methods: {
-    getSvgUrl(str) {
-      return str.split("").map(x => {
-        let cp = x.charCodeAt(0).toString(16);
-        // return `/kanji/0${cp}.svg`;
-        // return `https://cdn.rawgit.com/KanjiVG/kanjivg/r20160426/kanji/0${cp}.svg`;
-        return this.$axios.get(`/api/strokes?id=${cp}`);
-      });
-    },
-    async link(e, url) {
-      await url.then(r => {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(r.data.svg, "image/svg+xml");
-        var d = doc.documentElement;
-        e.appendChild(d);
-        // console.log(d);
-        const scheme = [
-          "1976D2",
-          "D32F2F",
-          "388E3C",
-          "FBC02D",
-          "512DA8",
-          "F57C00"
-        ];
-        const pathColors = scheme
-          .map(
-            (c, i) => `path:nth-child(${i + 1}) {
+    // getSvgUrl(str) {
+    //   return str.split("").map(x => {
+    //     let cp = x.charCodeAt(0).toString(16);
+    //     // return `/kanji/0${cp}.svg`;
+    //     // return `https://cdn.rawgit.com/KanjiVG/kanjivg/r20160426/kanji/0${cp}.svg`;
+    //     return this.$axios.get(`/api/strokes?id=${cp}`);
+    //   });
+    // },
+    link(e, r) {
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(r.data.svg, "image/svg+xml");
+      var d = doc.documentElement;
+      e.appendChild(d);
+      // console.log(d);
+      const scheme = [
+        "1976D2",
+        "D32F2F",
+        "388E3C",
+        "FBC02D",
+        "512DA8",
+        "F57C00"
+      ];
+      const pathColors = scheme
+        .map(
+          (c, i) => `path:nth-child(${i + 1}) {
           stroke: #${c};
         }`
-          )
-          .join("\n");
-        const timePerUnit = 0.01;
-        let startTime = 0;
-        const paths = d.querySelectorAll("path");
-        const pathAnimations = Array.prototype.map
-          .call(paths, path => {
-            const length = path.getTotalLength();
-            const id = path.id.replace(/(:|-)/g, "\\$1");
-            const duration = timePerUnit * length;
-            path.style.animationDelay = `${startTime}s`;
-            path.style.strokeDasharray = `${length}`;
-            path.style.animationDuration = `${duration}s`;
-            startTime += duration;
-            return `svg:hover #${id} {
+        )
+        .join("\n");
+      const timePerUnit = 0.01;
+      let startTime = 0;
+      const paths = d.querySelectorAll("path");
+      const pathAnimations = Array.prototype.map
+        .call(paths, path => {
+          const length = path.getTotalLength();
+          const id = path.id.replace(/(:|-)/g, "\\$1");
+          const duration = timePerUnit * length;
+          path.style.animationDelay = `${startTime}s`;
+          path.style.strokeDasharray = `${length}`;
+          path.style.animationDuration = `${duration}s`;
+          startTime += duration;
+          return `svg:hover #${id} {
             stroke-dashoffset: ${length};
           }`;
-          })
-          .join("\n");
-        const style = doc.createElementNS("http://www.w3.org/2000/svg", "style");
-        style.textContent = `
+        })
+        .join("\n");
+      const style = doc.createElementNS("http://www.w3.org/2000/svg", "style");
+      style.textContent = `
           ${pathColors}
           text {
             font-family: Noto, Roboto, sans-serif;
@@ -87,14 +90,12 @@ export default {
           }
           ${pathAnimations}
         `;
-        d.children[0].appendChild(style);
-      });
+      d.children[0].appendChild(style);
     }
   },
   mounted() {
-    this.urls = this.getSvgUrl(this.kanji);
-    this.urls.forEach((url,index) => {
-      this.link(this.$refs.object, url);
+    this.kanji.forEach((url, index) => {
+      this.link(this.$refs[`object${index}`][0], url);
     });
   },
   props: ["kanji"],
