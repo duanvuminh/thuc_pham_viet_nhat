a592497528ae9b36558ca81b37460ac1d4700512
vu minh duan <duanvuminh@gmail.com>
2021-03-21 16:00:54 +0900

    backup

--- layouts/oboe.vue
+++ layouts/oboe.vue
@@ -5,12 +5,30 @@
       <nuxt-link to="/forum" class="nuxt-link" :prefetch="false"
         >Forum</nuxt-link
       >
-      <v-btn v-if="!loggedIn" icon to="/login">
-        <v-icon small>{{mdiAccountOutline}}</v-icon>
+      <v-btn icon @click="show" ref="btnDot">
+        <v-icon>{{ mdiDotsVertical }}</v-icon>
       </v-btn>
-      <v-btn v-else icon to="/logout">
-        <v-icon small>{{mdiLogout }}</v-icon>
-      </v-btn>
+      <v-menu bottom left v-model="showMenu" absolute
+      offset-y :position-x="x"
+      :position-y="y">
+        <v-list>
+          <v-list-item v-if="!loggedIn" to="/login">
+            <v-list-item-title>Login</v-list-item-title>
+          </v-list-item>
+          <template v-if="loggedIn">
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              :to="item.link"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/logout">
+              <v-list-item-title>Logout</v-list-item-title>
+            </v-list-item>
+          </template>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-main>
       <v-container fluid>
@@ -31,7 +49,7 @@
 </template> 
 
 <script>
-import { mdiDotsVertical,mdiAccountOutline,mdiLogout  } from "@mdi/js";
+import { mdiDotsVertical} from "@mdi/js";
 import { mapState } from "vuex";
 //const HtmlParser = () => import("@/components/HtmlParser");
 import HtmlParser from "@/components/HtmlParser";
@@ -43,14 +61,15 @@ export default {
   data() {
     return {
       mdiDotsVertical,
-      mdiAccountOutline,
-      mdiLogout, 
       selectedText: "",
       selectedTextShow: "",
       selectedTextApi: {
         vi: "",
       },
       dialog: false,
+      showMenu:false,
+       x: 0,
+      y: 0,
     };
   },
   mounted() {
@@ -67,6 +86,15 @@ export default {
       }
       this.selectedText = text;
     },
+    show (e) {
+        e.preventDefault()
+        this.showMenu = false
+        this.x = e.clientX
+        this.y = e.clientY
+        this.$nextTick(() => {
+          this.showMenu = true
+        })
+      },
   },
   computed: {
     loggedIn() {
